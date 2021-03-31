7f090f1e73ac3b7c6e924337b8bd236304d2fcb1
Verónica Ojeda <glebapps@gmail.com>
2021-03-31 15:41:28 +0200

    add hero component

---
+++ components/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hero">
+    <h1 class="hero__title">{{ content.title }}</h1>
+    <p class="hero__subtitle">{{ content.subtitle }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  padding-top: 40px;
+
+  &__title {
+    @include mobileToDesktopFontSize(var(--fs-giant), var(--fs-colossal));
+
+    max-width: 480px;
+    margin-bottom: 24px;
+    font-weight: var(--fw-bold);
+    line-height: 1;
+  }
+
+  &__subtitle {
+    color: var(--fc-dimmed);
+    font-size: var(--fs-default);
+  }
+}
+</style>
