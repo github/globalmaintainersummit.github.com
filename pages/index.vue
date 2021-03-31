a119818d8be06f8896289d88e29a0ab8fcbd4bb3
Verónica Ojeda <glebapps@gmail.com>
2021-03-31 15:01:12 +0200

    add section mixin

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div style="display: grid; place-items: center; margin-top: 100px">
+  <div
+    class="main"
+    style="display: grid; place-items: center; margin-top: 100px"
+  >
     <h1>Open Source Maintainer Summit - June 7-9 2021</h1>
     <p>
       A virtual gathering for the maintainers that make open source possible.
@@ -8,6 +11,9 @@
 </template>
 
 <style lang="scss" scoped>
+.main {
+  @include section();
+}
 h1 {
   @include gradientTitle(var(--bg-gradient-start), var(--bg-gradient-end));
 }
