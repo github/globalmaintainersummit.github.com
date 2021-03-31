39f0ec3b77a65250789a79288c6135c3193319d3
Verónica Ojeda <glebapps@gmail.com>
2021-03-31 15:49:28 +0200

    add github logo

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="hero">
+    <GitHub />
     <h1 class="hero__title">{{ content.title }}</h1>
     <p class="hero__subtitle">{{ content.subtitle }}</p>
   </div>
 </template>
 
 <script>
+import GitHub from '~/assets/svg/github.svg'
+
 export default {
+  components: {
+    GitHub,
+  },
   props: {
     content: {
       type: Object,
@@ -18,12 +24,16 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
-  padding-top: 40px;
+  margin-top: 40px;
+  @media screen and (min-width: $screen-ipad) {
+    margin-top: 80px;
+  }
 
   &__title {
     @include mobileToDesktopFontSize(var(--fs-giant), var(--fs-colossal));
 
     max-width: 480px;
+    margin-top: 40px;
     margin-bottom: 24px;
     font-weight: var(--fw-bold);
     line-height: 1;
