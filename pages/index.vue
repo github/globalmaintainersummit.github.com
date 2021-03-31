fa78360e326ce8af4ac0354f426025a13b8cec25
Verónica Ojeda <glebapps@gmail.com>
2021-03-31 15:41:28 +0200

    add hero component

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,16 @@
 <template>
-  <div
-    class="main"
-    style="display: grid; place-items: center; margin-top: 100px"
-  >
-    <h1>{{ page.hero.title }}</h1>
-    <p>
-      {{ page.hero.subtitle }}
-    </p>
-    <div style="display: flex; flex-direction: column; gap: 16px">
-      <CustomLink href="#">Terms</CustomLink>
-      <CustomLink href="#" button>Nominate an Open Source Project</CustomLink>
-    </div>
+  <div class="main">
+    <Hero :content="hero" />
   </div>
 </template>
 
 <script>
 export default {
   async asyncData({ $content }) {
-    const page = await $content('home').fetch()
+    const { hero } = await $content('home').fetch()
 
     return {
-      page,
+      hero,
     }
   },
 }
@@ -29,8 +19,5 @@ export default {
 <style lang="scss" scoped>
 .main {
   @include section();
-}
-h1 {
-  @include gradientTitle(var(--bg-gradient-start), var(--bg-gradient-end));
 }
 </style>
