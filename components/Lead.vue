f1a5061bf6159733d11548b314fdc48e98f0e11c
Verónica Ojeda <39853718+glebapps@users.noreply.github.com>
2021-04-08 12:57:33 +0200

    feat: add event to calendar (#22)

--- components/Lead.vue
+++ components/Lead.vue
@@ -2,17 +2,68 @@
   <div>
     <div class="lead">
       <h2 class="lead__title">{{ content.title }}</h2>
-      <p class="lead__dates">{{ content.dates }}</p>
+      <CommonLink :href="calendarUrl">
+        <span class="lead__dates">{{ dates }}</span>
+        <AddToCalIcon />
+      </CommonLink>
     </div>
   </div>
 </template>
 
 <script>
+import AddToCalIcon from '~/assets/svg/add_to_cal.svg?inline'
+
 export default {
+  components: {
+    AddToCalIcon,
+  },
   props: {
     content: {
       type: Object,
       required: true,
+    },
+  },
+  data() {
+    return {
+      locale: 'en-US',
+      startDate: new Date(this.content.dates.start),
+      endDate: new Date(this.content.dates.end),
+    }
+  },
+  computed: {
+    dates() {
+      const startDay = this.startDate.getDate()
+      const startMonth = this.startDate.toLocaleDateString(this.locale, {
+        month: 'long',
+      })
+      const endDay = this.endDate.getDate()
+      const endMonth = this.endDate.toLocaleDateString(this.locale, {
+        month: 'long',
+      })
+      const year = this.startDate.getFullYear()
+
+      return startMonth === endMonth
+        ? `${startMonth} ${startDay}-${endDay}, ${year}`
+        : `${startMonth} ${startDay} - ${endMonth}${endDay}, ${year}`
+    },
+    calendarUrl() {
+      const startDate = this.formatDateCalendar(this.startDate)
+      // In order to set an all day event in Google Calendar, the end day has to be a day more of the actual date
+      // e.g. To create an event for dates June 8-9 it needs to be set June 8-10
+      const endDate = this.formatDateCalendar(this.nextDay(this.endDate))
+      const text = 'Global Maintainer Event'
+      const details =
+        'A virtual gathering for the maintainers that make open source possible'
+
+      return `http://www.google.com/calendar/event?action=TEMPLATE&dates=${startDate}%2F${endDate}&text=${text}&&details=${details}`
+    },
+  },
+  methods: {
+    formatDateCalendar(date) {
+      return date.toISOString().slice(0, 10).replaceAll('-', '') // YYYYMMDD
+    },
+    nextDay(date) {
+      return new Date(date.setDate(date.getDate() + 1))
     },
   },
 }
@@ -38,7 +89,9 @@ export default {
   &__dates {
     @include mobileToDesktopFontSize(var(--fs-regular), var(--fs-medium));
 
+    display: inline-block;
     margin-top: 40px;
+    margin-right: 16px;
     color: var(--fc-default);
     font-weight: var(--fw-medium);
     font-size: var(--fs-medium);
