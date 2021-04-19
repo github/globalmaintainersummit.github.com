ef0211dfaa05855635a5c4db01b2f72baf62e683
beagleknight <beagleknight@users.noreply.github.com>
2021-04-19 10:06:55 +0000

    deploy: 32666023ae91910b3259e1b7ac00cb454eaea3bb

---
+++ _nuxt/static/1618826812/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618826812",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b3b5b0ca"}}}}(null,"\u002F"));
