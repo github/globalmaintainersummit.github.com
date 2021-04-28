f2ddd75d7d314bee7375bb2c805c1f6177a5c68d
Verónica Ojeda <glebapps@gmail.com>
2021-04-28 12:57:20 +0200

    remove reusable components and use mixin instead

--- components/common/SelectCalendar.vue
+++ components/common/SelectCalendar.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    <CommonCustomSelect
-      :title="title"
-      :options="options"
-      label="Select a calendar to save the conference date"
-      show-icons
-    />
+  <div
+    class="select"
+    :class="showDropdown && 'select--open'"
+    role="listbox"
+    :aria-label="label"
+  >
+    <p
+      class="select__title"
+      tabindex="0"
+      @click="toggleDropdown"
+      @keydown.enter="toggleDropdown"
+      @keydown.space="toggleDropdown"
+    >
+      <span v-if="title"> {{ title }} </span>
+      <span
+        v-if="showDropdown"
+        class="select__close"
+        :tabindex="showDropdown && '0'"
+      >
+        ✖
+      </span>
+    </p>
+    <ul v-if="showDropdown">
+      <li
+        v-for="option in options"
+        :key="option"
+        :tabindex="showDropdown && '0'"
+      >
+        {{ option }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -13,9 +37,35 @@
 export default {
   data() {
     return {
-      title: 'Save the date',
-      options: ['apple', 'google', 'outlook'],
+      showDropdown: false,
+      label: 'Select a calendar to save the conference date',
+      title: 'Save the Date',
+      options: ['apple', 'yahoo', 'google'],
     }
+  },
+  methods: {
+    toggleDropdown() {
+      this.showDropdown = !this.showDropdown
+    },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.select {
+  cursor: pointer;
+  &--open {
+    @include select();
+  }
+
+  &__title {
+    display: flex;
+    margin-top: 0;
+    column-gap: 8px;
+  }
+
+  &__close {
+    cursor: pointer;
+  }
+}
+</style>
