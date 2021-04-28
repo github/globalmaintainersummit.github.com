66b6d8f504346a8ceba331ceceea9d99615b7018
Verónica Ojeda <glebapps@gmail.com>
2021-04-28 11:46:33 +0200

    add basic custom select

---
+++ components/common/CustomSelect.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="select">
+    <span class="select__title">{{ title }}</span>
+    <ul>
+      <li v-for="option in options" :key="option">{{ option }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      default: null,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    showIcons: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.select {
+  padding: 24px;
+  background: var(--bg-default);
+  border-radius: 12px;
+  box-shadow: 0 16px 24px 0 rgba(13, 9, 16, 0.08),
+    0 8px 16px 0 rgba(13, 9, 16, 0.12);
+
+  &__title {
+    display: block;
+    padding-bottom: 8px;
+  }
+}
+
+ul {
+  list-style-type: none;
+  margin-block: 0;
+  padding-inline: 0;
+}
+
+li {
+  padding: 10px 0;
+}
+</style>
