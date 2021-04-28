400384b30fb956ab17d36179ad247a100f5a1453
Verónica Ojeda <glebapps@gmail.com>
2021-04-28 11:47:06 +0200

    add temporary page to display the components

---
+++ pages/design.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <CommonSelectCalendar />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 100px;
+}
+</style>
