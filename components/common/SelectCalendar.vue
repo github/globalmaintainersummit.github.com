3d076e8bc05093ab32c6bf1f04ed50fdd38ca1d8
Verónica Ojeda <glebapps@gmail.com>
2021-04-28 11:46:44 +0200

    add basic calendar and year selectors

---
+++ components/common/SelectCalendar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <CommonCustomSelect
+      :title="title"
+      :options="options"
+      label="Select a calendar to save the conference date"
+      show-icons
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Save the date',
+      options: ['apple', 'google', 'outlook'],
+    }
+  },
+}
+</script>
---
+++ components/common/SelectYear.vue
@@ -0,0 +1,13 @@
+<template>
+  <Select :options="options" label="Select the conference year" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: ['2021', '2020'],
+    }
+  },
+}
+</script>
