b31e899017083c254aa937dd0a477f10c6b7b366
Verónica Ojeda <glebapps@gmail.com>
2021-05-04 16:40:15 +0200

    add dummy button to index

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,12 @@
     <h1 style="display: flex; margin-top: 100px; place-content: center">
       Work in progress
     </h1>
+    <CommonCustomButton href="https://vuejs.org/v2/guide/components-slots.html">
+      <template #text>All Maintainers</template>
+    </CommonCustomButton>
+    <!-- <CommonButton to="#" icon-prefix>
+      <template #text>Sponsor PyLadies</template>
+    </CommonButton> -->
   </section>
 </template>
 
