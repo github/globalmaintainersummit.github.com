58129bd2bbf9af2c195936d01fe37f41c8391f46
lastpotion <89.aitor.porro@gmail.com>
2021-05-05 12:44:18 +0200

    Add some css comments.

--- components/common/Link.vue
+++ components/common/Link.vue
@@ -63,26 +63,34 @@ export default {
     position: absolute;
     bottom: -18px;
     left: 0;
-    width: 100%;
     height: 24px;
+
+    /** Draw a circle at the center and displaced it up 10px
+     Then draw a transparent 100% circle less -4px that will act as a "border"
+     and another one with color that extrudes thos 4 pixels (3+1) to make it smother on both sides.
+     form there to the rest of the backgroudn extend the transparency. */
     background: radial-gradient(
           52% 100% at 50% -10px,
-          #0000 calc(100% - 4px),
+          transparent calc(100% - 4px),
           var(--underline-color) calc(100% - 3px) calc(100% - 1px),
-          #0000
+          transparent
         ) -18px 100%,
-      radial-gradient(
+      /** Draw a circle at the top center and displaced it to the bottom + 10px
+      and follow the exact instrucitions as the previous one. */
+        radial-gradient(
           52% 100% at 50% calc(100% + 10px),
-          #0000 calc(100% - 4px),
+          transparent calc(100% - 4px),
           var(--underline-color) calc(100% - 3px) calc(100% - 1px),
-          #0000
+          transparent
         )
         0 calc(100% - 12px);
     background-repeat: repeat-x;
+    // Width and height of a single wave
     background-size: 36px 16px;
     cursor: pointer;
     transition: 0.5s ease, mask-position 0s 0.5s;
     content: '';
+    // Add a mask to hide the whole gradient that can be animated to be removed.
     mask: linear-gradient(#fff 0 0) top / 100% calc(100% - 18px),
       linear-gradient(#fff 0 0) var(--p, 0) 100% / var(--d, 0) 18px;
     mask-repeat: no-repeat;
