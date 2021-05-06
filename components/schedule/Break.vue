1082e172826abc7f09f5a330642b62a25d6f7eb9
Verónica Ojeda <glebapps@gmail.com>
2021-05-06 17:30:23 +0200

    add break component

---
+++ components/schedule/Break.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="break">
+    <p>Break</p>
+    <CommonWave class="underline" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.break {
+  display: flex;
+  align-items: center;
+  color: var(--fc-accent);
+  font-family: var(--ff-title);
+  column-gap: 20px;
+}
+</style>
