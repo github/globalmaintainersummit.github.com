b0bb322d648a131cf8a1dd2648510d0f00338b21
Verónica Ojeda <glebapps@gmail.com>
2021-05-07 13:11:05 +0200

    add discussion component

--- components/schedule/Discussion.vue
+++ components/schedule/Discussion.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
+  <div class="discussion">
+    <CommonWave />
     <p>Discussions</p>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.discussion {
+  display: flex;
+  flex-direction: column;
+  p {
+    @include mobileToDesktopFontSize(var(--fs-small), var(--fs-medium));
+
+    margin: 16px 0;
+  }
+}
+</style>
