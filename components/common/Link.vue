014e7ac0f7f5216cad7ce85169adafc83418791a
Verónica Ojeda <glebapps@gmail.com>
2021-05-07 15:58:29 +0200

    extract common wave styles to a mixin

--- components/common/Link.vue
+++ components/common/Link.vue
@@ -52,35 +52,16 @@ export default {
 
 <style lang="scss" scoped>
 .link {
-  position: relative;
+  @include wave($bottom: -22px);
+
   display: inline-block;
   font-weight: var(--fw-semi-bold);
   font-size: var(--fs-small);
   text-decoration: none;
+
   &::after {
-    position: absolute;
-    bottom: -22px;
-    left: 0;
-    width: 100%;
-    height: 24px;
-    background: radial-gradient(
-          52% 100% at 50% -12px,
-          transparent calc(100% - 5px),
-          var(--underline-color) calc(100% - 4px) calc(100% - 1px),
-          transparent
-        ) -18px 100%,
-      radial-gradient(
-          52% 100% at 50% calc(100% + 12px),
-          transparent calc(100% - 5px),
-          var(--underline-color) calc(100% - 4px) calc(100% - 1px),
-          transparent
-        )
-        0 calc(100% - 15px);
-    background-repeat: repeat-x;
-    background-size: 36px 20px;
     cursor: pointer;
     transition: 0.5s ease, mask-position 0s 0.5s;
-    content: '';
     mask: linear-gradient(#fff 0 0) top / 100% calc(100% - 24px),
       linear-gradient(#fff 0 0) var(--p, 0) 100% / var(--d, 0) 24px;
     mask-repeat: no-repeat;
--- components/common/Wave.vue
+++ components/common/Wave.vue
@@ -4,36 +4,10 @@
 
 <style lang="scss" scoped>
 .wave {
+  @include wave($bottom: -20px);
+
   position: relative;
-  top: -8px;
+  top: -6px;
   width: 100%;
-  content: '';
-  &::after {
-    position: absolute;
-    bottom: -20px;
-    left: 0;
-    width: 100%;
-    height: 24px;
-    background: radial-gradient(
-          52% 100% at 50% -10px,
-          transparent calc(100% - 4px),
-          var(--underline-color) calc(100% - 3px) calc(100% - 1px),
-          transparent
-        ) -18px 100%,
-      radial-gradient(
-          52% 100% at 50% calc(100% + 10px),
-          transparent calc(100% - 4px),
-          var(--underline-color) calc(100% - 3px) calc(100% - 1px),
-          transparent
-        )
-        0 calc(100% - 12px);
-    background-repeat: repeat-x;
-    background-size: 36px 16px;
-    transition: 0.5s ease, mask-position 0s 0.5s;
-    content: '';
-    mask: linear-gradient(#fff 0 0) top / 100% calc(100% - 18px),
-      linear-gradient(#fff 0 0) 100% 100% / 100% 18px;
-    mask-repeat: no-repeat;
-  }
 }
 </style>
