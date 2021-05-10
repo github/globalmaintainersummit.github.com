2f002fef4b4c4aedb6bb62331a5d4bc45c0cd883
Verónica Ojeda <glebapps@gmail.com>
2021-05-10 12:21:04 +0200

    add all components for code of conduct page

---
+++ components/codeOfConduct/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h4>{{ content.title }}</h4>
+    <p>{{ content.description }}</p>
+    <CodeOfConductList
+      v-for="(behavior, index) in content.goodAndBadBehaviors"
+      :key="index"
+      :description="behavior.description"
+      :examples="behavior.examples"
+    />
+    <div v-html="content.finalTips"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ pages/code-of-conduct.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <CodeOfConductPill :content="intro" />
+    <CodeOfConductMain :content="codeOfConduct" />
+    <CodeOfConductPill :content="reportingIncident" />
+    <CodeOfConductPill :content="credit" />
+  </div>
+</template>
+
+<script>
+import socialMetadata from '~/mixins/social-metadata'
+
+export default {
+  mixins: [socialMetadata],
+  async asyncData({ $content }) {
+    const { intro, codeOfConduct, reportingIncident, credit } = await $content(
+      '2021/pages/code-of-conduct'
+    ).fetch()
+    return {
+      intro,
+      codeOfConduct,
+      reportingIncident,
+      credit,
+    }
+  },
+}
+</script>
