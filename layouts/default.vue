9af606b86e7936de38681c92d2b8c1e8307cf7d9
lastpotion <89.aitor.porro@gmail.com>
2021-05-10 11:21:36 +0200

    Fix navigation on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <TopNav />
+  <div :class="{ 'open-nav': navActive }">
+    <TopNav :active="navActive" @toggle="toggleNav" />
     <main class="main">
       <Nuxt />
     </main>
@@ -8,6 +8,20 @@
     <Footer />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      navActive: false,
+    }
+  },
+  methods: {
+    toggleNav(value) {
+      this.navActive = value
+    },
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 .main {
@@ -15,5 +29,11 @@
   z-index: var(--z-index-content);
   width: 100%;
   overflow: hidden;
+}
+.open-nav {
+  @media screen and (max-width: $screen-sm) {
+    height: 100vh;
+    overflow: hidden;
+  }
 }
 </style>
