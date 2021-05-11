6794284ec5ec568ae944196b04330b8b992d5633
Verónica Ojeda <glebapps@gmail.com>
2021-05-11 10:55:17 +0200

    add profile page (wip)

--- pages/maintainer/_profile.vue
+++ pages/maintainer/_profile.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>hi</div>
+  <section class="maintainer">
+    <CommonCustomButton
+      icon="arrow-left"
+      theme="transparent"
+      class="maintainer__go-back"
+      @click="goBack"
+    >
+      Go back
+    </CommonCustomButton>
+    <div class="maintainer__image">
+      <MaintainerImage
+        :handler="maintainer.handler"
+        :name="maintainer.profile.name"
+      />
+    </div>
+    <div class="maintainer__details">
+      <MaintainerProfile
+        :handler="maintainer.handler"
+        :profile="maintainer.profile"
+      />
+    </div>
+  </section>
 </template>
 
 <script>
@@ -14,5 +35,30 @@ export default {
 
     return { maintainer }
   },
+  methods: {
+    goBack() {
+      this.$router.back()
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.maintainer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  padding: 0 24px;
+  background-image: url('~/assets/svg/bg_pills/graphic_pills.svg');
+  background-repeat: no-repeat;
+  background-position: top;
+  &__go-back {
+    align-self: flex-start;
+  }
+  &__details {
+    max-width: 720px;
+  }
+}
+</style>
