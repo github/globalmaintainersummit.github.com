656d6d56d04583eaff8fe9d102c5687bbee110c2
Verónica Ojeda <glebapps@gmail.com>
2021-05-11 10:54:34 +0200

    add maintainer profile image

---
+++ components/maintainer/Image.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="image">
+    <img
+      :src="require(`~/assets/img/maintainers/${handler}.jpg`)"
+      :alt="name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    handler: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.image {
+  --maintainer-width: 876px;
+  --maintainer-height: 284px;
+  --maintainer-border: 5px;
+
+  position: relative;
+  width: calc(var(--maintainer-width) + var(--maintainer-border));
+  height: calc(var(--maintainer-height) + var(--maintainer-border));
+
+  img {
+    position: relative;
+    top: 0;
+    left: 0;
+    z-index: 2;
+    width: calc(var(--maintainer-width) + var(--maintainer-border));
+    height: calc(var(--maintainer-height) + var(--maintainer-border));
+    object-fit: cover;
+    object-position: center;
+    border: var(--maintainer-border) white solid;
+    transition: top 0.25s ease-in-out, left 0.25s ease-in-out;
+  }
+  &::after {
+    position: absolute;
+    top: 19px;
+    left: 19px;
+    z-index: 1;
+    display: block;
+    width: var(--maintainer-width);
+    height: var(--maintainer-height);
+    background-color: var(--bg-dimmed);
+    content: '';
+    -webkit-mask-image: url('~/assets/svg/pattern_shadow.svg');
+    mask-image: url('~/assets/svg/pattern_shadow.svg');
+  }
+}
+</style>
