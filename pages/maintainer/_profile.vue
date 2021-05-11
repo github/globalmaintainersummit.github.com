6af546b9c572c26f218d9c4f20d926e9c9eb26f3
Verónica Ojeda <glebapps@gmail.com>
2021-05-11 09:25:57 +0200

    redirect mainters to their profile and retrieve their info

---
+++ pages/maintainer/_profile.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>hi</div>
+</template>
+
+<script>
+import socialMetadata from '~/mixins/social-metadata'
+
+export default {
+  mixins: [socialMetadata],
+  async asyncData({ $content, params }) {
+    const maintainer = await $content(
+      `2021/shared/maintainers/${params.profile}`
+    ).fetch()
+
+    return { maintainer }
+  },
+}
+</script>
