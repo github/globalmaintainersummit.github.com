309e11dacf9700c648726796a456638fa78b8e57
Verónica Ojeda <glebapps@gmail.com>
2021-05-20 18:09:55 +0200

    componetise home

---
+++ components/home/Content.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="content">
+    <div class="content--normal">
+      <h2>{{ content.title1 }}</h2>
+      <div v-html="content.block1" />
+    </div>
+    <div class="content--highlighted">
+      <h2>{{ content.title2 }}</h2>
+      <div v-html="content.block2" />
+      <div class="save">
+        <CommonCalendarDropdown type="button" class="save-button" />
+        <CommonWave class="save-decoration" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  position: relative;
+  &--normal {
+    max-width: 786px;
+    margin: 0 auto;
+    margin-top: 120px;
+    h2 {
+      max-width: 786px;
+    }
+    div {
+      max-width: 671px;
+      font-family: var(--ff-default);
+      line-height: 1.5;
+    }
+  }
+  &--highlighted {
+    @include highlightedBox();
+
+    max-width: 902px;
+    margin: 0 auto;
+    margin-top: 200px;
+    padding: 80px 120px;
+
+    h2 {
+      @include mobileToDesktopFontSize(var(--fs-giant), var(--fs-colossal));
+
+      max-width: 671px;
+      margin-top: 0;
+      margin-bottom: 48px;
+      line-height: 1;
+    }
+  }
+}
+.save {
+  display: flex;
+  align-items: center;
+  max-width: 786px;
+  margin: 80px auto 0;
+  &-button {
+    min-width: 300px;
+  }
+  &-decoration {
+    position: absolute;
+    left: 30px;
+    min-width: 1000px;
+    @media only screen and (min-width: 680px) {
+      left: 100px;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,12 @@
 <template>
-  <section class="home">
-    <div class="home__decoration" aria-hidden="true">
+  <section>
+    <div class="decoration" aria-hidden="true">
       <img src="~/assets/svg/bg_pills/graphic_hero.svg" alt="" />
     </div>
     <HomeHero :content="hero" />
     <HomeNewsletter :content="newsletter" />
-    <div class="home__content">
-      <div class="home__content--normal">
-        <h2>{{ content.title1 }}</h2>
-        <div v-html="content.block1" />
-      </div>
-      <div class="home__content--highlighted">
-        <h2>{{ content.title2 }}</h2>
-        <div v-html="content.block2" />
-        <div class="home__save">
-          <CommonCalendarDropdown type="button" class="home__save-button" />
-          <CommonWave class="home__save-decoration" />
-        </div>
-      </div>
-    </div>
-    <div class="home__maintainers">
-      <h3>{{ maintainersContent.title }}</h3>
-      <CommonGrid :maintainers="maintainers" three-cols data-cy="maintainers" />
-      <div class="home__maintainers-button">
-        <CommonCustomButton to="/maintainers" icon="arrow-right" icon-suffix>
-          {{ maintainersContent.cta }}
-        </CommonCustomButton>
-      </div>
-    </div>
+    <HomeContent :content="content" />
+    <HomeMaintainers :content="maintainersContent" :maintainers="maintainers" />
   </section>
 </template>
 
@@ -59,76 +38,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.home {
+section {
   @include section();
 
   position: relative;
-  &__decoration {
-    position: absolute;
-    top: -105px;
-    left: -110px;
-    width: 100%;
-  }
-  &__content {
-    position: relative;
-    &--normal {
-      max-width: 786px;
-      margin: 0 auto;
-      margin-top: 120px;
-      h2 {
-        max-width: 786px;
-      }
-      div {
-        max-width: 671px;
-        font-family: var(--ff-default);
-        line-height: 1.5;
-      }
-    }
-    &--highlighted {
-      @include highlightedBox();
-
-      max-width: 902px;
-      margin: 0 auto;
-      margin-top: 200px;
-      padding: 80px 120px;
-
-      h2 {
-        @include mobileToDesktopFontSize(var(--fs-giant), var(--fs-colossal));
-
-        max-width: 671px;
-        margin-top: 0;
-        margin-bottom: 48px;
-        line-height: 1;
-      }
-    }
-  }
-  &__save {
-    display: flex;
-    align-items: center;
-    max-width: 786px;
-    margin: 80px auto 0;
-    &-button {
-      min-width: 300px;
-    }
-    &-decoration {
-      position: absolute;
-      left: 30px;
-      min-width: 1000px;
-      @media only screen and (min-width: 680px) {
-        left: 100px;
-      }
-    }
-  }
-  &__maintainers {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    margin-top: 200px;
-    &-button {
-      align-self: center;
-      margin-top: 70px;
-      margin-bottom: 10px;
-    }
-  }
+}
+.decoration {
+  position: absolute;
+  top: -105px;
+  left: -110px;
+  width: 100%;
 }
 </style>
