0177d8a124e70a3b4b05237da97c899bd678242b
Verónica Ojeda <glebapps@gmail.com>
2021-05-26 18:12:34 +0200

    update hamburger menu icon

--- components/Hamburger.vue
+++ components/Hamburger.vue
@@ -19,44 +19,46 @@ export default {
 
 <style lang="scss" scoped>
 .burger {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
   span {
     position: relative;
     top: 0;
     display: block;
-    width: 28px;
-    height: 3px;
-    margin: 3px 0;
+    width: 33px;
+    height: 4px;
+    margin-bottom: 5px;
     background: var(--bg-hamburger);
     border-radius: 3px;
+    transform-origin: 4px 0;
     cursor: pointer;
-    transition: transform 0.2s linear, top 0.15s ease-in-out 0.3s,
-      color 0.25s ease 0.2s, width 0.25s ease 0.2s;
+    transition: transform 0.5s cubic-bezier(0.77, 0.2, 0.05, 1),
+      background 0.5s cubic-bezier(0.77, 0.2, 0.05, 1), opacity 0.55s ease;
+    &:first-child {
+      transform-origin: 0% 0%;
+    }
     &:nth-child(2) {
       width: 18px;
     }
     &:nth-child(3) {
-      width: 8px;
+      width: 10px;
+      transform-origin: 0% 100%;
     }
   }
   &--active {
+    display: block;
     span {
       background: var(--bg-hamburger--open);
-      &:nth-child(1) {
-        top: 6px;
-        transform: rotate(45deg);
-        transition: top 0.2s linear, transform 0.15s ease 0.3s,
-          color 0.25s ease 0.2s;
-      }
+      transform: rotate(45deg) translate(-2px, -1px);
+      opacity: 1;
       &:nth-child(2) {
-        width: 0;
-        transition: color 0.25s ease 0.2s;
+        transform: rotate(0deg) scale(0.2, 0.2);
+        opacity: 0;
       }
       &:nth-child(3) {
-        top: -6px;
-        width: 28px;
-        transform: rotate(-45deg);
-        transition: top 0.2s linear, transform 0.15s ease 0.3s,
-          color 0.25s ease 0.2s;
+        width: 33px;
+        transform: rotate(-45deg) translate(-2px, 0);
       }
     }
   }
