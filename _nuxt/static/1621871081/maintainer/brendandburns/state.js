299bf5d6e65f6bbce5e389dda4ea0b63d9a198a2
glebapps <glebapps@users.noreply.github.com>
2021-05-27 15:49:04 +0000

    deploy: 59588f6f4be86b0b903ab0d180a1ac7193430ba7

--- _nuxt/static/1621871081/maintainer/brendandburns/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621871081",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmaintainer\u002Fbrendandburns",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4fdc4bc8"}}}}(null));
