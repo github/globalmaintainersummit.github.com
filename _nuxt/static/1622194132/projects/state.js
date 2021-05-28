eb784b6a633608d3cce2e965e43eaa9ca07430b3
glebapps <glebapps@users.noreply.github.com>
2021-05-28 12:25:13 +0000

    deploy: f294cc70f6a7a2b956187a97cee67faf078bdf7c

--- _nuxt/static/1622194132/projects/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622194132",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1dd2e171"}}}}(null));
