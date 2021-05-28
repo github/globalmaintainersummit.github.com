0e9efe7bb0839e7f04a02fc70123308de4534b56
glebapps <glebapps@users.noreply.github.com>
2021-05-28 09:28:55 +0000

    deploy: e813c2151afb7b42a4ddc373d7ff68e512c03dde

---
+++ _nuxt/static/1622194132/maintainer/webchick/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622194132",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmaintainer\u002Fwebchick",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1dd2e171"}}}}(null));
