a80af67c91f3a4a06d6cd224dd548716d8331ff1
Verónica Ojeda <glebapps@gmail.com>
2021-06-01 13:17:50 +0200

    add logs for debug

--- pages/schedule.vue
+++ pages/schedule.vue
@@ -26,6 +26,7 @@ export default {
     }
   },
   mounted() {
+    console.log('schedule mounted')
     this.$store.dispatch('setUserTimezone')
   },
 }
--- plugins/user-timezone.js
+++ plugins/user-timezone.js
@@ -1,5 +1,6 @@
 export default ({ app }, inject) => {
   // Inject user timezone in Vue, context and store.
+  console.log('user-timezone')
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
   inject('timezone', () => timeZone)
 }
