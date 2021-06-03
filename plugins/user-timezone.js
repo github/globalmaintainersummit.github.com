3a5139f801fd2c96a019019338e85c3eaa6d4c17
Verónica Ojeda <glebapps@gmail.com>
2021-06-03 12:02:26 +0200

    add hack to be able to change timezone for e2e tests

--- plugins/user-timezone.js
+++ plugins/user-timezone.js
@@ -1,5 +1,7 @@
-export default ({ app }, inject) => {
+export default ({ app, store }, inject) => {
   // Inject user timezone in Vue, context and store.
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
   inject('timezone', () => timeZone)
+  window.updateTimeZone = (timeZone) =>
+    store.commit('updateUserTimeZone', timeZone)
 }
