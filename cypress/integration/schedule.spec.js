37ab15889dc9c1e4f5570d9b8981fc428918b7b9
Verónica Ojeda <glebapps@gmail.com>
2021-06-03 12:41:15 +0200

    fix e2e tests

--- cypress/integration/schedule.spec.js
+++ cypress/integration/schedule.spec.js
@@ -12,7 +12,7 @@ describe('Schedule', () => {
     cy.window().then((window) => {
       window.updateUserTimeZone('Asia/Magadan')
       cy.get('[data-cy=timezoneSwitch]').within(() => {
-        cy.get('[data-cy=option2]').click()
+        cy.get('[data-cy=option2]').click({ force: true })
       })
       cy.contains('Wednesday ⌁ June 9, 2021')
       cy.contains('Thursday ⌁ June 10, 2021')
@@ -26,7 +26,7 @@ describe('Schedule', () => {
         const pdtTimeFirstSlot = $times[0].firstElementChild.innerText
 
         cy.get('[data-cy=timezoneSwitch]').within(() => {
-          cy.get('[data-cy=option2]').click()
+          cy.get('[data-cy=option2]').click({ force: true })
         })
 
         cy.get('[data-cy=slot] > [data-cy=slotTime]').then(($times) => {
