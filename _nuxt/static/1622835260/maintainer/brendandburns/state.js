9429e4c0ee5da4215392eb17a14a61ac8a404412
josepjaume <josepjaume@users.noreply.github.com>
2021-06-04 19:34:22 +0000

    deploy: 81e6811824dadeb0ad0bb06d048a62de399d9478

---
+++ _nuxt/static/1622835260/maintainer/brendandburns/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622835260",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmaintainer\u002Fbrendandburns",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8cb74192"}}}}(null));
---
+++ _nuxt/static/1622835260/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622835260",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8cb74192"}}}}(null,"\u002F"));
