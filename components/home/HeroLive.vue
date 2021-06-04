a2358c9a4abbc95a04ef0fa8fd8dd74364104b91
Verónica Ojeda <glebapps@gmail.com>
2021-06-04 13:10:34 +0200

    extract video to a component

--- components/home/HeroLive.vue
+++ components/home/HeroLive.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="hero">
-    <client-only placeholder="Loading...">
-      <youtube class="hero__video" :video-id="videoId"></youtube>
-      <div class="hero__video-decoration"></div>
-    </client-only>
+    <HomeStreamingVideo :video-id="videoId" />
     <h1>
       {{ content.title }}
     </h1>
@@ -46,42 +43,12 @@ export default {
 
 <style lang="scss" scoped>
 .hero {
-  --video-width: 256px;
-  --video-height: 192px;
-
   position: relative;
   max-width: 1440px;
   margin: 0 auto;
   padding-top: 24px;
-  @media only screen and (min-width: $screen-xs) {
-    --video-width: 320px;
-    --video-height: 192px;
-  }
-  @media only screen and (min-width: 600px) {
-    --video-width: 320px;
-    --video-height: 192px;
-  }
   @media only screen and (min-width: $screen-sm) {
-    --video-width: 480px;
-    --video-height: 272px;
-
     padding-top: 40px;
-  }
-  @media only screen and (min-width: $screen-tablet) {
-    --video-width: 640px;
-    --video-height: 360px;
-  }
-  @media only screen and (min-width: $screen-xl) {
-    --video-width: 854px;
-    --video-height: 480px;
-  }
-  @media only screen and (min-width: 1200px) {
-    --video-width: 1024px;
-    --video-height: 600px;
-  }
-  @media only screen and (min-width: $screen-max) {
-    --video-width: 1280px;
-    --video-height: 720px;
   }
   h1 {
     @include mobileToDesktopFontSize(var(--fs-medium), var(--fs-giant));
@@ -90,38 +57,6 @@ export default {
     max-width: 836px;
     margin-top: 56px;
     line-height: 1;
-  }
-  &__video {
-    position: relative;
-    z-index: 2;
-    ::v-deep iframe {
-      width: var(--video-width);
-      height: var(--video-height);
-      transition: all 0.3s ease-in;
-    }
-    &-decoration {
-      position: absolute;
-      top: 40px;
-      left: 16px;
-      width: var(--video-width);
-      height: var(--video-height);
-      transition: all 0.5s ease-in;
-      @media only screen and (min-width: $screen-sm) {
-        top: 58px;
-      }
-      &::after {
-        z-index: 1;
-        display: block;
-        width: var(--video-width);
-        height: var(--video-height);
-        overflow: hidden;
-        background-color: var(--bg-dimmed);
-        transition: background-color 0.25s ease, top 0.25s ease, left 0.25s ease;
-        content: '';
-        -webkit-mask-image: url('~/assets/svg/pattern_shadow.svg');
-        mask-image: url('~/assets/svg/pattern_shadow.svg');
-      }
-    }
   }
   &__lead {
     display: flex;
