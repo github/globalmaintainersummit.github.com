fecdee439db9bd166b38b864352ad06cc46410de
Verónica Ojeda <glebapps@gmail.com>
2021-06-07 12:53:22 +0200

    remove e2e test as the hero will be different depending on manual deploys

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,13 +3,6 @@ describe('Home', () => {
     cy.visit('/')
   })
 
-  it('displays the hero lead and the correct dates/', () => {
-    cy.contains(
-      'A virtual gathering for the maintainers who make open source possible'
-    )
-    cy.contains('June 8-9, 2021')
-  })
-
   it('Maintainers are listed and there are no duplicated maintainers in the Maintainers list', () => {
     cy.get('[data-cy=maintainers]').then(($maintainers) => {
       let maintainers = []
